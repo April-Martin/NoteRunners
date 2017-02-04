56ebf6006f0d1e5ca53caaab6e5f0dd08c121c59
April Martin <nandra001@gmail.com>
2017-02-04 16:52:24 -0700

    Got collision detection, speed modification working
    WE DIDN'T ATTACH THE STUPID SCRIPT
    GRRRRRAAAAAAAAAAAAAHHHHHHHHHHHRGRGRG

--- NoteRunners Main Project/Assets/Scripts/GameController.cs
+++ NoteRunners Main Project/Assets/Scripts/GameController.cs
@@ -9,37 +9,36 @@ public class GameController : MonoBehaviour {
     public PlayerMovement Player;
     public float CameraSpeed = 0.5f;
 
+    internal float speedMultiplier = 1f;
+
 
 	// Use this for initialization
 	void Start () 
     {
-        InvokeRepeating("spawnWall", 0f, 2f);
+        InvokeRepeating("spawnWall", 0f, 3f);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         movePlayerAndCamera();
-
-        
 	}
 
     void spawnWall()
     {
 
-        Vector3 newWallWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, 0f, 5f));
+        Vector3 newWallWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, 0f, 10f));
         newWallWorldPosition.z = 0;
         Debug.Log(newWallWorldPosition);
         GameObject instantiatedWall = Instantiate(Wall, newWallWorldPosition, Quaternion.identity);
         //instantiatedWall.transform.position = new Vector3(newWallWorldPosition.x, instantiatedWall.transform.position.y, 0f);
-
 
         //yield break;
     }
 
     void movePlayerAndCamera()
     {
-        Vector3 deltaPos = (Vector3.right * CameraSpeed * Time.deltaTime);
+        Vector3 deltaPos = (Vector3.right * CameraSpeed * speedMultiplier * Time.deltaTime);
         Camera.main.transform.position += deltaPos;
         Player.transform.position += deltaPos;
     }
---
+++ NoteRunners Main Project/Assets/Scripts/Hoop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hoop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("hello");
+
+        if (col.gameObject.name == "Player")
+        {
+            Debug.Log("HOOP");
+            float speedMultiplierReference = GameObject.Find("Game Controller").GetComponent<GameController>().speedMultiplier;
+
+            GameObject.Find("Game Controller").GetComponent<GameController>().speedMultiplier += 1f;
+        }
+    }
+
+}
