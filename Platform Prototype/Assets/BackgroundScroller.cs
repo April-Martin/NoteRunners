e3e3fb98156329c8850317288aa09fd97f40e857
April-Martin <nandra001@gmail.com>
2017-03-09 19:56:29 -0700

    Cheesed the scrolling background. Added prefabs before everything breaks again.

---
+++ Platform Prototype/Assets/BackgroundScroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour {
+
+	public float speed = 1f;
+
+	float temp = 0f;
+
+	MeshRenderer renderer;
+
+	// Use this for initialization
+	void Start ()
+	{
+		renderer = GetComponent<MeshRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		temp += speed * Time.deltaTime;
+		renderer.material.mainTextureOffset = new Vector2 (Mathf.Repeat (temp, 1), 0);
+	}
+}
