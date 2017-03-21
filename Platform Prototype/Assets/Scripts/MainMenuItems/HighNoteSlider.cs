35e3d7577299ba39d792d803ccd6da72833070d5
Michael A. Frogget <mfrogget@gmail.com>
2017-03-20 20:06:08 -0600

    Globals Update
    Button and Slider Functionality to global adjustment variables are
    functional.

--- Platform Prototype/Assets/Scripts/MainMenuItems/HighNoteSlider.cs
+++ Platform Prototype/Assets/Scripts/MainMenuItems/HighNoteSlider.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HighNoteSlider : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Slider slider;
+
+    // Use this for initialization
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void changeValue()
+    {
+        slider.value = GameGlobals.GlobalInstance.getHighNoteIndex();
+    }
 }
